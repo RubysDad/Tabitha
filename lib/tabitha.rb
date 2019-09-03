91b0dce388db5f674995f2b3b82aaeff5b926305
Rubysdad <marktravismorales@gmail.com>
2019-09-03 13:38:27 -0500

    Added Array class

--- lib/tabitha.rb
+++ lib/tabitha.rb
@@ -1,8 +1,10 @@
 require 'tabitha/version'
+require 'tabitha/array'
 
 module Tabitha
   class Application
     def call(env)
+      # `echo debug > debug.txt`;
       [200, { 'Content-Type' => 'text/html' },
        ['Hello from the Tabitha Framework']]
     end
