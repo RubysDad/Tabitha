5d54016db15fec80251f57375045bf0d93f619b3
Rubysdad <marktravismorales@gmail.com>
2019-09-02 23:24:52 -0500

    Making Tabitha use Rack

--- lib/tabitha.rb
+++ lib/tabitha.rb
@@ -1,6 +1,10 @@
-require "tabitha/version"
+require 'tabitha/version'
 
 module Tabitha
-  class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, { 'Content-Type' => 'text/html' },
+       ['Hello from the Tabitha Framework']]
+    end
+  end
 end
